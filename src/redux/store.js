536196b5ed1b6510e0cfde7f9a0944a81d377bdd
Saurab Prasai <saurablovetocode@gmail.com>
2024-03-04 21:09:25 +0545

    redux persist added to save the cart items to local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import cartReducer from './cart/cartSlice.js'
-export const store=configureStore({
-    reducer:{
-            cart:cartReducer
-    }
+import { persistStore,persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer=combineReducers({
+    cart:cartReducer
 })
+
+const persistConfig={
+    key:"root",
+    storage
+}
+
+const persistedReducer=persistReducer(persistConfig,rootReducer)
+
+export const store=configureStore({
+    reducer:persistedReducer,
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware({serializableCheck:false})
+})
+
+export const persistor=persistStore(store)
