203e7c29ab426143ad951051037df3baeb583a99
Saurab Prasai <saurablovetocode@gmail.com>
2024-03-04 08:47:19 +0545

    complete add to cart redux functionality

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,11 @@
+import { useSelector } from "react-redux"
 
 
 export default function Cart() {
+
+  const cartProduct=useSelector(state=>state.cart.products)
+
+  console.log(cartProduct);
 
   return (
     <div>
--- src/redux/cart/cartSlice.js
+++ src/redux/cart/cartSlice.js
@@ -1,24 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState={
-    products:[],
-}
+const initialState = {
+  products: [],
+};
 
-export const cartSlice=createSlice({
-    name:"cart",
-    initialState,
-    reducers:{
-        addToCart:(state,action)=>{
-            state.products=[...state.products,action.payload]
-        },
-        removeFromCart:(state,action)=>{
-            const cartProducts=state.products.filter((product)=>product!=action.payload)
-            state.products=cartProducts;
-        }
-    }
-})
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      // checking if the product already exist in cart
+      let existingProduct = state.products.findIndex(
+        (product) => product.id == action.payload.id
+      );
 
+      if (existingProduct != -1) {
+        const cart = [...state.products];
+        // index of existing product in cart
+        const findIndex = cart.findIndex(
+          (product) => product.id === action.payload.id
+        );
+        cart[findIndex].count += 1;
+        state.products = [...cart];
+      } else {
+        action.payload.count = 1;
+        state.products = [...state.products, action.payload];
+      }
+    },
+    removeFromCart: (state, action) => {
+      const cartProducts = state.products.filter(
+        (product) => product != action.payload
+      );
+      state.products = cartProducts;
+    },
+  },
+});
 
-export const {addToCart,removeFromCart}=cartSlice.actions
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
-export default cartSlice.reducer
+export default cartSlice.reducer;
